842dd0611089f890e657b37b6680038f35d84607
saipatil <saipatilv777@gmail.com>
2020-11-29 18:01:17 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,4 @@
+import time
+from plyer import notification
+if __name__ == "__main__":
+    notification.notify(title="Please Drink the water",message="The U.S. National Academies of Sciences, Engineering, and Medicine determined that an adequate daily fluid intake is: About 15.5 cups (3.7 liters) of fluids a day for men. About 11.5 cups (2.7 liters) of fluids a day for women",timeout=600)
